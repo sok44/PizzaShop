14729a7f805a540cc81257ad8aa2164d947d6852
Sok444 <sok_88@mail.ru>
2017-11-02 22:47:52 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -21,4 +21,23 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 
 	window.localStorage.setItem(key, x);
+
+	alert('Items in your cart:' + cart_get_number_of_items());
+}
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+
+		if (key.indexOf('product_') == 0)
+		{
+			var value = window.localStorage.getItem(key);
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
 }
